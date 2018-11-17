354e11db7d319e8c8bd08b6e37a0ee30e9acac0d
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-11-17 14:34:28 +0530

    Remove Tools and Other extra info from projects card

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -5,12 +5,7 @@ export default [
     type: 'react',
     link: 'https://github.com/kaushiknishchay/WeatherApp',
     desc:
-      `
-        <b>Responsibility</b> :
-        A react native app which uses the DarkSky weather API to show weather info at the current location (using GPS).
-        <br><br>
-        <b>Technologies Used : </b>
-        React Native, Style Components
+      `A react native app which uses the DarkSky weather API to show weather info at the current location (using GPS).
         `,
   },
   {
@@ -18,12 +13,7 @@ export default [
     position: 'FullStack Developer',
     type: 'nodejs',
     desc:
-      `
-        <b>Responsibility</b> :
-        A slack bot with NodeJS backend, which gives trending videos list and allows to search videos inside slack app.
-        <br><br>
-        <b>Technologies Used : </b>
-        Node.JS
+      `A slack bot with NodeJS backend, which gives trending videos list and allows to search videos inside slack app.
         `,
   },
   {
@@ -31,12 +21,7 @@ export default [
     position: 'FullStack Developer',
     type: 'nodejs',
     desc:
-      `
-        <b>Responsibility</b> :
-        A realtime node js project which shows order and notifies when new order are placed in real time.
-        <br><br>
-        <b>Technologies Used : </b>
-        Node.JS, React.JS, Socket.IO
+      `A realtime node js project which shows order and notifies when new order are placed in real time.
         `,
   },
   {
@@ -45,13 +30,8 @@ export default [
     type: 'react',
     link: 'https://github.com/kaushiknishchay/React-Native-Restaurant-App',
     desc:
-      `
-        <b>Responsibility</b> :
-        An internal CodeBrahma React Native + React demo app showcasing the logic
+      `An internal CodeBrahma React Native + React demo app showcasing the logic
         code-sharing between the mobile and web app.
-        <br><br>
-        <b>Technologies Used : </b>
-        React.JS, React Native, Styled Components, Redux, Redux Sagas
         `,
   }, {
     name: 'Opening Bell',
@@ -59,14 +39,9 @@ export default [
     type: 'react',
     link: 'https://github.com/kaushiknishchay/ShareMarketIndia',
     desc:
-      `
-        <b>Responsibility</b> :
-        A react native app which list stocks and indices from NSE & BSE (India).<br>
-        Shows charts for Indices/Stocks<br>
-        Search the stocks and Save favorite Stocks<br>
-        <br><br>
-        <b>Technologies Used : </b>
-        React.JS, React Native, Styled Components, Redux, Redux Sagas, React Charts
+      `A react native app which list stocks and indices from NSE & BSE (India).
+        Shows charts for Indices/Stocks.
+        Search the stocks and Save favorite Stocks.
         `,
   }, {
     name: 'Indian Stock Exchange',
@@ -74,169 +49,102 @@ export default [
     link: 'https://github.com/kaushiknishchay/indian-stock-exchange',
     type: 'nodejs',
     desc:
-      `
-            <b>Responsibility</b> :
-            A NPM package which provides an interface to fetch data from india's national stock exchange for the equities and indices.
-            <br><br>
-            <b>Technologies Used : </b>
-            JavaScript
-        `,
+      `A NPM package which provides an interface to fetch data from india's national stock exchange for the equities and indices.
+            `,
   }, {
     name: 'CB React Components',
     position: 'React native developer',
     type: 'react',
     desc:
-      `
-            <b>Responsibility</b> :
-            An internal CodeBrahma React Native app with reusable and customizable components.
-            <br><br>
-            <b>Technologies Used : </b>
-            React.JS, React Native, Styled Components, Redux, Redux Sagas
-        `,
+      `An internal CodeBrahma React Native app with reusable and customizable components.
+            `,
   }, {
     name: 'Hacker News App',
     position: 'React native developer',
     type: 'react',
     link: 'https://github.com/kaushiknishchay/hackerNewsApp',
     desc: `
-          <b>Responsibility</b> :
+          
           React Native app built using the
           Hacker News API
           to show Top, New and Best stories.
-          <br><br>
-          <b>Technologies Used : </b>
-          React.JS, React Native, RxJS, Redux,
-          Reselect, Normalizr`,
+          `,
   }, {
     name: 'Github API',
     position: 'React developer',
     type: 'react',
     link: 'https://github.com/kaushiknishchay/Github-API-React',
-    desc: `
-          <b>Responsibility</b> :<br>
-          React Web app built which<br>
+    desc: `React Web app built which<br>
           - List public events for the signed-in user.<br>
           - Allows repository and user searching.<br>
-          - List public events for un-auth user<br>
+          - List public events for un-auth user
           Write test cases for the components.
-          <br><br>
-          <b>Technologies Used : </b>
-          React.JS, RxJS, Redux,
-          Reselect, Normalizr`,
+        `,
   }, {
     name: 'React + Firebase Todo App',
     position: 'React + Firebase developer',
     type: 'firebase',
     link: 'https://github.com/kaushiknishchay/Firebase-Todo-App',
-    desc: `
-            <b>Responsibility</b> :
-            React web todo app connected to realtime firebase database, which saves the created todos to the db.
-            <br><br>
-            <b>Technologies Used : </b>
-            React.JS, Firebase, Redux, styled-components`,
+    desc: `React web todo app connected to realtime firebase database, which saves the created todos to the db.
+            `,
   }, {
     name: 'Cloud Backup App',
     position: 'React native developer',
     type: 'react',
     link: 'https://github.com/kaushiknishchay/heyCloudyReactNative',
-    desc: `
-            <b>Responsibility</b> :
-            React Native app which backup photos on device to a local server using a scheduled service.
-            <br><br>
-            <b>Technologies Used : </b>
-            React.JS, React Native, RxJS, Redux, Redux Sagas, Realm`,
+    desc: `React Native app which backup photos on device to a local server using a scheduled service.
+            `,
   }, {
     name: 'Siebel CRM',
     position: 'Siebel Developer',
     type: 'siebel',
-    desc: `<b>Responsibility</b> :
+    desc: `
                 Created Workflows, WebServices etc. and worked on OpenUI.`,
   }, {
     name: 'Hand Gesture Recognition',
     position: 'Programmer',
     type: 'python',
-    desc: `<b>Details</b> :
-                Detecting Hand Gesture and movement by using OpenCV library for Python and perform actions based on
+    desc: `Detecting Hand Gesture and movement by using OpenCV library for Python and perform actions based on
 the gesture detected e.g., Mouse Left or Right Click.
-          <br><br>
-          <b>Programming Language</b> : Python`,
+         `,
   }, {
     name: 'PicSquare',
     position: 'Android Developer',
     type: 'android',
     desc: `
-            <b>Details</b> :
              A sample Android Application which
              allows to convert images in square.
              Developed as a part to test Image
              APIs of android and for knowledge purpose.
-          <br><br>
-          <b>Tools Used</b> :
-             Android Studio
-          <br><br>
-          <b>Programming Language</b> : Java
           `,
   }, {
     name: 'MobiGo',
     position: 'Android Developer',
     type: 'android',
-    desc: `
-            <b>Details</b> :
-             MobiGo was an Android App Store which allowed users to download android apps. Created the app using Java and integrated various 3rd Party libraries to enhance UI and functionality. Created REST API in PHP.
-          <br><br>
-          <b>Tools Used</b> :
-             Android Studio, PHPStorm, Notepad++, HeidiSQL
-          <br><br>
-          <b>Programming Language</b> :
-              Java
+    desc: `MobiGo was an Android App Store which allowed users to download android apps. Created the app using Java and integrated various 3rd Party libraries to enhance UI and functionality. Created REST API in PHP.
           `,
   }, {
     name: 'DealsUpto',
     position: 'FullStack Developer (PHP)',
     type: 'php',
-    desc: `
-            <b>Details</b> :
-             Created a full fledged website which allows creating posts regarding various offers, in simple terms a blog post. Created an admin Panel to allow modification or addition of various data, viz. Categories, Posts, User Comments etc.
-          <br><br>
-          <b>Tools Used</b> :
-             PHPStorm, Notepad++
-          <br><br>
-          <b>Technologies Used</b> :
-              PHP, MySQL, HTML/CSS/JS, BootStrap
+    desc: `Created a full fledged website which allows creating posts regarding various offers, in simple terms a blog post. Created an admin Panel to allow modification or addition of various data, viz. Categories, Posts, User Comments etc.
           `,
   }, {
     name: 'PicturesOfJoy',
     position: 'FullStack Developer (PHP)',
     type: 'php',
-    desc: `
-            <b>Details</b> :
-              PicturesOfJoy was a photograph showcasing
+    desc: `PicturesOfJoy was a photograph showcasing
               website. Its frontend was build using
               React.JS + Redux, for Backend PHP Laravel
               framework was used. It also has an Admin
               Panel created using the same, and has token
               based authentication.
-          <br><br>
-          <b>Tools Used</b> :
-             WebStorm, PHPStorm, Notepad++, HeidiSQL
-          <br><br>
-          <b>Technologies Used</b> :
-              PHP, MySQL, React.JS, Redux, ES6 JavaScript
           `,
   }, {
     name: 'Kaushik.tech',
     position: 'Web Developer (React)',
     type: 'react',
     link: 'https://github.com/kaushiknishchay/kaushiknishchay.github.io',
-    desc: `
-            <b>Details</b> :
-             Responsive Personal Website created using React.JS
-          <br><br>
-          <b>Tools Used</b> :
-             WebStorm
-          <br><br>
-          <b>Technologies Used</b> :
-              React.JS, HTML/CSS/JS
-         `,
+    desc: 'Responsive Personal Website created using React.JS. Set-Up Code-Splitting and Lazy Loading for components.',
   },
 ];
