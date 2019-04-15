bf990a53c5e24350a202ef9ccd7698c9f157ecb8
SHolmes <kaushiknishchay@gmail.com>
2019-04-15 23:22:28 +0530

    Fix Wrong start date for HackerRank Job
    Use string template

--- src/sections/FooterSection.jsx
+++ src/sections/FooterSection.jsx
@@ -7,20 +7,12 @@ const FooterSection = ({ children }) => (
   <footer className="footer-section">
     {children}
     <div className="disclaimer">
-
-
       Disclaimer: Images used on the website belong to their respective owners.
     </div>
     <p>
-
-
       &copy; kaushik.tech
       <span>
-
-
-          2017 -
-        {' '}
-        {(new Date()).getFullYear()}
+      {`2017 - ${(new Date()).getFullYear()}`}
       </span>
     </p>
   </footer>
