ecfdad9a0da7f2b5462af2b80a271dda76e1e500
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-14 11:59:43 +0000

    Deploy kaushiknishchay/kaushiknishchay.github.io to github.com/kaushiknishchay/kaushiknishchay.github.io.git:master

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -6,8 +6,11 @@ import Loading from '../Loading';
 const Timeline = React.lazy(() => import(/* webpackChunkName: "Timeline" */ '../components/Timeline'));
 
 const ExperienceSection = () => (
-  <section id="experiences" className="light--content"
-           aria-label="Professional Experiences section">
+  <section
+    id="experiences"
+    className="light--content"
+    aria-label="Professional Experiences section"
+  >
     <h3 className="content--title">Experiences</h3>
     <React.Suspense fallback={<Loading />}>
       <Timeline data={experiencesList} />
