7ca7f5dfc462a8ba256e1ec646eec92dcd7a8808
kaushiknishchay <kaushiknishchay@gmail.com>
2021-01-16 12:26:35 +0000

    Deploying to gh-pages from  @ 3bb4584e83030cbde7135c038f38cefdbeb8be87 🚀

---
+++ precache-manifest.2463605dd9c1878163cc80a1a3273c3d.js
@@ -0,0 +1,87 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f36fa146fa6da5848fa52de80147bfa",
+    "url": "./index.html"
+  },
+  {
+    "url": "./js/main.bundle.ae2dc485de17f7eaf123.js"
+  },
+  {
+    "url": "./js/npm.react-dom.4af42de6f75a6705b99d.js"
+  },
+  {
+    "url": "./js/npm.react.bf16070256c702ad62a1.js"
+  },
+  {
+    "revision": "ae2dc485de17f7eaf123",
+    "url": "app.aa26ab39175074bf3817.css"
+  },
+  {
+    "revision": "a38a1b932c1aea9724019a280f48d8e8",
+    "url": "assets/github.svg"
+  },
+  {
+    "revision": "b6f367a681464fb352b3c9e67a2a5139",
+    "url": "assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "4d42a09be2fc4074acc6353e56fe5a6d",
+    "url": "assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "6d903add49c27af8b3189f3d127b5800",
+    "url": "assets/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "5cab6653840f313d3937da402ef150aa",
+    "url": "assets/icons/icon-128x128.png"
+  },
+  {
+    "revision": "999473c7ef9104aaccf8cf37825387bc",
+    "url": "assets/icons/icon-144x144.png"
+  },
+  {
+    "revision": "24c09682713db1a572c6275a1d782ab4",
+    "url": "assets/icons/icon-152x152.png"
+  },
+  {
+    "revision": "8a05aa96e23f2f186b176c5cb78b41c5",
+    "url": "assets/icons/icon-192x192.png"
+  },
+  {
+    "revision": "e991a81f724601aa153acabfa85f7b25",
+    "url": "assets/icons/icon-36x36.png"
+  },
+  {
+    "revision": "fef244e05432803f6161219c6b701a1f",
+    "url": "assets/icons/icon-384x384.png"
+  },
+  {
+    "revision": "e97b7a20c9ab662ba99062b8eaf522cd",
+    "url": "assets/icons/icon-48x48.png"
+  },
+  {
+    "revision": "8b994226397d1e906e870824abbf0bb8",
+    "url": "assets/icons/icon-512x512.png"
+  },
+  {
+    "revision": "8be9fade2e0a0f4d88aa826ad487380b",
+    "url": "assets/icons/icon-72x72.png"
+  },
+  {
+    "revision": "13689bd8c453f36a910acddc3bf7f1ff",
+    "url": "assets/icons/icon-96x96.png"
+  },
+  {
+    "revision": "23c5041ab992081a9d4264ba8679e9d3",
+    "url": "assets/instagram.svg"
+  },
+  {
+    "revision": "c13bb41bbbeb065897dc1b62f10c5516",
+    "url": "assets/linkedin.svg"
+  },
+  {
+    "revision": "8c14a8ebc8c5920b5ae8e602b6b1a57d",
+    "url": "assets/person_bg.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,33 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.2463605dd9c1878163cc80a1a3273c3d.js"
+);
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg)$/, new workbox.strategies.CacheFirst({ "cacheName":"images", plugins: [new workbox.expiration.Plugin({ maxEntries: 20, purgeOnQuotaError: false })] }), 'GET');
+workbox.routing.registerRoute(/\.(?:css|js)$/, new workbox.strategies.CacheFirst({ "cacheName":"js-css", plugins: [new workbox.expiration.Plugin({ maxEntries: 10, purgeOnQuotaError: false })] }), 'GET');
